d2d0bce692e921d89b48ebf05cd786b8bd8af77d
Alexander Shelikhov <shelikhov.a@nmarket.pro>
2026-02-16 19:12:01 +0300

    Initial Commit

---
+++ AI Advent Challenge/AI_Advent_ChallengeApp.swift
@@ -0,0 +1,17 @@
+//
+//  AI_Advent_ChallengeApp.swift
+//  AI Advent Challenge
+//
+//  Created by Александр Шелихов on 16.02.2026.
+//
+
+import SwiftUI
+
+@main
+struct AI_Advent_ChallengeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ AI Advent Challenge/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  AI Advent Challenge
+//
+//  Created by Александр Шелихов on 16.02.2026.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
